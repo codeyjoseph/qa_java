a6ae9886f929a0514f6fd9697fabc606f836c675
Joe K <kearneyjoe2@outlook.com>
2024-10-23 16:28:30 +0100

    io streaming shenanigans

--- src/main/java/org/lbg/c4/App.java
+++ src/main/java/org/lbg/c4/App.java
@@ -1,19 +1,33 @@
 package org.lbg.c4;
 
-import org.lbg.unreachable.ClassB;
+import javax.sound.midi.Soundbank;
+import java.io.IOException;
 
 /**
  * Hello world!
  */
 public class App {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        ClassA ca = new ClassA();
-        System.out.println(ca.toString());
-        ClassB cb = new ClassB();
-        System.out.println(cb.age);
-        int x = 33;
-        System.out.println("Hello world! " + x);
+    public static void main(String[] args) throws IOException {
+        int counter = 0;
+        int b;
+        try {
+            while ((b = System.in.read()) != -1) {
+                System.out.println(b);
+                counter++;
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        System.out.println("No. of chars in ASCII: " + counter);
+        try {
+            while ((b = System.in.read()) != -1) {
+                System.out.println((char)b);
+                counter++;
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        System.out.println("No. of chars in alpha: " + counter);
     }
 }
